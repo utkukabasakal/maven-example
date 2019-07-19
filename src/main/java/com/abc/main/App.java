6346e2775f26a515064f096c6cb6a0e7b140d4db
Murat Utku KABASAKALOĞLU <utkukabasakal58@gmail.com>
2019-07-19 11:33:34 +0300

    maven example

---
+++ src/main/java/com/abc/main/App.java
@@ -0,0 +1,10 @@
+package com.abc.main;
+public class App {
+    public static void main( String[] args ) {
+        DortIslem dortIslem = new DortIslem();
+        System.out.println(dortIslem.topla(7,8));
+        System.out.println(dortIslem.cikar(15,9));
+        System.out.println(dortIslem.carp(12,4));
+        System.out.println(dortIslem.bol(45,9));
+    }
+}
---
+++ src/main/java/com/abc/main/DortIslem.java
@@ -0,0 +1,16 @@
+package com.abc.main;
+
+public class DortIslem {
+    public float topla(float sayi1, float sayi2) {
+        return sayi1 + sayi2;
+    }
+    public float cikar(float sayi1, float sayi2) {
+        return sayi1 - sayi2;
+    }
+    public float carp(float sayi1, float sayi2) {
+        return sayi1 * sayi2;
+    }
+    public float bol(float sayi1, float sayi2) {
+        return sayi1 / sayi2;
+    }
+}
